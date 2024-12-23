973cacb67f9a81c5086a5c172a0e54f8cdc54d6f
Govindarajan <rdgovindarajan@gmail.com>
2024-12-23 19:42:01 +0530

    Added webpack and node modules for the front-end. Multiple files (HMTL, CSS and JS) will be generated as a single HTML file for use within the application

---
+++ Utils for PBI/FrontendWeb/webpack.config.js
@@ -0,0 +1,27 @@
+﻿const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body'
+        }),
+        new HTMLInlineScriptPlugin(),
+    ],
+    mode: 'production',
+}
--- Utils for PBI/Services/GenerateLineagePage.cs
+++ Utils for PBI/Services/GenerateLineagePage.cs
@@ -29,7 +29,7 @@ namespace Utils_for_PBI.Services
 
             string appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "UtilsPBI");
             Directory.CreateDirectory(appDataPath);
-            HTMLFileLocation = Path.Combine(appDataPath, "Lineage.html");
+            HTMLFileLocation = Path.Combine(appDataPath, "index.html");
 
             // Write the HTML content to the file
             File.WriteAllText(HTMLFileLocation, HTMLContent);
