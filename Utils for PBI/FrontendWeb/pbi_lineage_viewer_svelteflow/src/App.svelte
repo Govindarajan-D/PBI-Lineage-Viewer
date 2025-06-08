07d261fd25046efc149417ca63d3ccc4d0f2a70c
Govindarajan <rdgovindarajan@gmail.com>
2025-06-09 01:04:20 +0530

    Added BFS to find ancestors and descendants

--- Utils for PBI/FrontendWeb/pbi_lineage_viewer_svelteflow/src/App.svelte
+++ Utils for PBI/FrontendWeb/pbi_lineage_viewer_svelteflow/src/App.svelte
@@ -1,182 +1,200 @@
 <script lang="ts">
-import dagre from '@dagrejs/dagre';
-import {
-  SvelteFlow,
-  Background,
-  Position,
-  ConnectionLineType,
-  Panel,
-  type Node,
-  type Edge,
-  type NodeEventWithPointer,
-} from '@xyflow/svelte';
+  import dagre from "@dagrejs/dagre";
+  import {
+    SvelteFlow,
+    Background,
+    Position,
+    ConnectionLineType,
+    Panel,
+    type Node,
+    type Edge,
+    type NodeEventWithPointer,
+  } from "@xyflow/svelte";
 
-import { writable } from 'svelte/store';
-import { onMount } from 'svelte';
-import CalcNode from './CalcNode.svelte';
-import ContextMenu from './ContextMenu.svelte';
-import '@xyflow/svelte/dist/style.css';
+  import { writable } from "svelte/store";
+  import { onMount } from "svelte";
+  import CalcNode from "./CalcNode.svelte";
+  import ContextMenu from "./ContextMenu.svelte";
+  import "@xyflow/svelte/dist/style.css";
 
-
-const nodeTypes = {
-  selectorNode: CalcNode,
-};
-
-const nodes = writable([]);
-const edges = writable([]);
-
-const dagreGraph = new dagre.graphlib.Graph();
-dagreGraph.setDefaultEdgeLabel(() => ({}));
-
-const nodeWidth = 172;
-const nodeHeight = 36;
-
-function getLayoutedElements(nodes: Node[], edges: Edge[], direction = 'TB') {
-  const isHorizontal = direction === 'LR';
-  dagreGraph.setGraph({ rankdir: direction });
-
-  nodes.forEach((node) => {
-    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-  });
-
-  edges.forEach((edge) => {
-    dagreGraph.setEdge(edge.source, edge.target);
-  });
-
-  dagre.layout(dagreGraph);
-
-  const layoutedNodes = nodes.map((node) => {
-    const nodeWithPosition = dagreGraph.node(node.id);
-    node.targetPosition = isHorizontal ? Position.Left : Position.Top;
-    node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    return {
-      ...node,
-      position: {
-        x: nodeWithPosition.x - nodeWidth / 2,
-        y: nodeWithPosition.y - nodeHeight / 2,
-      },
-    };
-  });
-
-  return { nodes: layoutedNodes, edges };
-}
-
-const position = { x: 0, y: 0 };
-const edgeType = 'smoothstep';
-
-const baseURL = "http://localhost:8080/utilspbi/api/";
-
-var svelte_nodes, svelte_edges;
-
-async function fetchdata() {
-const nodesURL = baseURL + "sveltenodes";
-const edgesURL = baseURL + "svelteedges";
-
-return Promise.all([
-    fetch(nodesURL).then(response => response.json()),
-    fetch(edgesURL).then(response => response.json())
-])
-    .then(([nodes, edges]) => {
-        nodes.forEach(element => {
-            if (element.data && element.data.name) {
-                element.data.nameLength = element.data.name.length;
-            }
-        });
-        svelte_nodes = nodes;
-        svelte_edges = edges;
-    });
-}
-
-onMount(() => {
-fetchdata().then(() => {
-  const layoutedElements = getLayoutedElements(svelte_nodes, svelte_edges, 'LR');
-  console.log('Layouted Nodes:', layoutedElements);
-  nodes.set(layoutedElements.nodes);
-  edges.set(layoutedElements.edges);
-})
-});
-
-
-
-function onLayout(direction: string) {
-  const layoutedElements = getLayoutedElements(svelte_nodes, svelte_edges, direction);
-
-  nodes.set(layoutedElements.nodes);
-  edges.set(layoutedElements.edges);
-}
-
-function filterNode(){
-  var filter_id = "goals";
-  var filtered_nodes = svelte_nodes.filter(node => node.id === filter_id);
-  var filtered_edges = svelte_edges.filter(edge => edge.source === filter_id || edge.target === filter_id);
-  console.log('Filtered Nodes:', filtered_nodes);
-  console.log('Filtered Edges:', filtered_edges);
-  //const layoutedElements = getLayoutedElements(svelte_nodes, svelte_edges, 'LR');
-
-  //nodes.set(layoutedElements.nodes);
-  //edges.set(layoutedElements.edges);
-}
-
-
-// Handling context menu
-let menu: {
-id: string;
-top?: number;
-left?: number;
-right?: number;
-bottom?: number;
-} | null = $state(null);
-let clientWidth: number = $state();
-let clientHeight: number = $state();
-
-
-const handleContextMenu: NodeEventWithPointer = ({ event, node }) => {
-  // Prevent native context menu from showing
-  event.preventDefault();
-
-  // Calculate position of the context menu. We want to make sure it
-  // doesn't get positioned off-screen.
-  menu = {
-    id: node.id,
-    top: event.clientY < clientHeight - 200 ? event.clientY : undefined,
-    left: event.clientX < clientWidth - 200 ? event.clientX : undefined,
-    right:
-      event.clientX >= clientWidth - 200
-        ? clientWidth - event.clientX
-        : undefined,
-    bottom:
-      event.clientY >= clientHeight - 200
-        ? clientHeight - event.clientY
-        : undefined,
+  const nodeTypes = {
+    selectorNode: CalcNode,
   };
-};
 
-// Close the context menu if it's open whenever the window is clicked.
-function handlePaneClick() {
-  menu = null;
-}
+  const nodes = writable([]);
+  const edges = writable([]);
+
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+  const nodeWidth = 172;
+  const nodeHeight = 36;
+
+  // This function is used to layout the nodes and edges using the dagre library.
+  // It sets the graph direction, adds nodes and edges to the graph, and then computes the layout.
+  function getLayoutedElements(nodes: Node[], edges: Edge[], direction = "TB") {
+    
+    //Define constants for the graph layout
+    const rankSep = 250; // Space between ranks
+    
+    const isHorizontal = direction === "LR";
+    dagreGraph.setGraph({ rankdir: direction, ranksep: rankSep });
+
+    nodes.forEach((node) => {
+      dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+    });
+
+    edges.forEach((edge) => {
+      dagreGraph.setEdge(edge.source, edge.target);
+    });
+
+    dagre.layout(dagreGraph);
+
+    const layoutedNodes = nodes.map((node) => {
+      const nodeWithPosition = dagreGraph.node(node.id);
+      node.targetPosition = isHorizontal ? Position.Left : Position.Top;
+      node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
+
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the Svelte Flow node anchor point (top left).
+      return {
+        ...node,
+        position: {
+          x: nodeWithPosition.x - nodeWidth / 2,
+          y: nodeWithPosition.y - nodeHeight / 2,
+        },
+      };
+    });
+
+    return { nodes: layoutedNodes, edges };
+  }
+
+  const position = { x: 0, y: 0 };
+  const edgeType = "smoothstep";
+
+  const baseURL = "http://localhost:8080/utilspbi/api/";
+
+  var svelte_nodes, svelte_edges;
+
+  // Fetches nodes and edges data from the API and processes it.
+  // It is asynchronous and returns a promise that resolves when the data is fetched.
+  async function fetchdata() {
+    const nodesURL = baseURL + "sveltenodes";
+    const edgesURL = baseURL + "svelteedges";
+
+    return Promise.all([
+      fetch(nodesURL).then((response) => response.json()),
+      fetch(edgesURL).then((response) => response.json()),
+    ]).then(([nodes, edges]) => {
+      nodes.forEach((element) => {
+        if (element.data && element.data.name) {
+          element.data.nameLength = element.data.name.length;
+        }
+      });
+      svelte_nodes = nodes;
+      svelte_edges = edges;
+    });
+  }
+
+  // The async function fetchdata is called when the component is mounted.
+  onMount(() => {
+    fetchdata().then(() => {
+      const layoutedElements = getLayoutedElements(
+        svelte_nodes,
+        svelte_edges,
+        "LR",
+      );
+      console.log("Layouted Nodes:", layoutedElements);
+      nodes.set(layoutedElements.nodes);
+      edges.set(layoutedElements.edges);
+    });
+  });
+
+  function onLayout(direction: string) {
+    const layoutedElements = getLayoutedElements(
+      svelte_nodes,
+      svelte_edges,
+      direction,
+    );
+
+    nodes.set(layoutedElements.nodes);
+    edges.set(layoutedElements.edges);
+  }
+
+  function filterNode() {
+    var filter_id = "goals";
+    var filtered_nodes = svelte_nodes.filter((node) => node.id === filter_id);
+    var filtered_edges = svelte_edges.filter(
+      (edge) => edge.source === filter_id || edge.target === filter_id,
+    );
+
+    const layoutedElements = getLayoutedElements(
+      filtered_nodes,
+      filtered_edges,
+      "LR",
+    );
+
+    nodes.set(layoutedElements.nodes);
+    edges.set(layoutedElements.edges);
+  }
+
+  // Handling context menu
+  let menu: {
+    id: string;
+    top?: number;
+    left?: number;
+    right?: number;
+    bottom?: number;
+  } | null = $state(null);
+  let clientWidth: number = $state();
+  let clientHeight: number = $state();
+
+  const handleContextMenu: NodeEventWithPointer = ({ event, node }) => {
+    // Prevent native context menu from showing
+    event.preventDefault();
+
+    // Calculate position of the context menu. We want to make sure it
+    // doesn't get positioned off-screen.
+    menu = {
+      id: node.id,
+      top: event.clientY < clientHeight - 200 ? event.clientY : undefined,
+      left: event.clientX < clientWidth - 200 ? event.clientX : undefined,
+      right:
+        event.clientX >= clientWidth - 200
+          ? clientWidth - event.clientX
+          : undefined,
+      bottom:
+        event.clientY >= clientHeight - 200
+          ? clientHeight - event.clientY
+          : undefined,
+    };
+  };
+
+  // Close the context menu if it's open whenever the window is clicked.
+  function handlePaneClick() {
+    menu = null;
+  }
 </script>
-<div style="height:100vh;" bind:clientWidth bind:clientHeight>
-<SvelteFlow
-  bind:nodes={$nodes}
-  bind:edges={$edges}
-  {nodeTypes}
-  fitView
-  onnodecontextmenu={handleContextMenu}
-  onpaneclick={handlePaneClick}
-  connectionLineType={ConnectionLineType.SmoothStep}
-  defaultEdgeOptions={{ type: 'bezier', animated: false }}
 
->
-  <Panel position="top-right">
-    <button onclick={() => onLayout('TB')}>vertical layout</button>
-    <button onclick={() => onLayout('LR')}>horizontal layout</button>
-    <button onclick={filterNode}>Filter Nodes</button>
-  </Panel>
-  <Background />
+<div style="height:100vh;" bind:clientWidth bind:clientHeight>
+  <SvelteFlow
+    bind:nodes={$nodes}
+    bind:edges={$edges}
+    {nodeTypes}
+    fitView
+    onnodecontextmenu={handleContextMenu}
+    onpaneclick={handlePaneClick}
+    connectionLineType={ConnectionLineType.SmoothStep}
+    defaultEdgeOptions={{ type: "bezier", animated: false }}
+  >
+    <Panel position="top-right">
+      <button onclick={() => onLayout("TB")}>vertical layout</button>
+      <button onclick={() => onLayout("LR")}>horizontal layout</button>
+      <button onclick={filterNode}>Filter Nodes</button>
+    </Panel>
+
+    <Background />
+    <!-- Context Menu (Right-Click menu) -->
     {#if menu}
       <ContextMenu
         onclick={handlePaneClick}
@@ -187,5 +205,5 @@ function handlePaneClick() {
         bottom={menu.bottom}
       />
     {/if}
-</SvelteFlow>
+  </SvelteFlow>
 </div>
---
+++ Utils for PBI/FrontendWeb/pbi_lineage_viewer_svelteflow/src/utility.ts
@@ -0,0 +1,47 @@
+
+// Breadth-First Search (BFS) to find all predecessors/successors of a node in a directed graph
+export function bfs(edges, startNode, direction = 'incomers' ){
+    const queue = [startNode];
+    const visited = new Set();
+    const hierNodes = new Set();
+
+    while (queue.length > 0) {
+        // shift does something similar to pop but on the first element
+        // if we had used pop, it would have been dfs ;)
+        const currentNode = queue.shift();
+        // Let's take a -> b -> d, e
+        // Let's walk back from `e`. The source of `e` will be `b`. 
+        // The startNode will be [e]. If the direction is incomers, we check where `e` is in target
+        // and then we filter to those nodes. Then for each of those neighboring nodes
+        // we loop through to find the next predecessor. Here for `e`, the source is `b`.
+        // We push b to the queue so that we can repeat the same steps. 
+        // We also push b to the hier Nodes. 
+
+        if (!visited.has(currentNode)){
+            visited.add(currentNode);
+            const neighbors = edges.filter(edge => 
+                direction === "incomers"? edge.target === currentNode : edge.source === currentNode
+            );
+            for (const neighbor of neighbors){
+                const nextNode = direction === "incomers"? neighbor.source : neighbor.target;
+                if(!visited.has(nextNode)){
+                    queue.push(nextNode);
+                    hierNodes.add(nextNode);
+                }
+            }
+
+        }
+    }
+
+    return hierNodes;
+}
+
+// Call BFS to find ancestors
+export function getAncestors(edges, startNode){
+    return bfs(edges, startNode, "incomers");
+}
+
+// Call BFS to find descendants
+export function getDescendants(edges, startNode){
+    return bfs(edges, startNode, "outgoers");
+}
