453c4216ee2e4bc29d7872fe71902aa1d0e30a02
Govindarajan-D <rdgovindarajan@gmail.com>
2025-08-20 01:11:50 +0530

    WIP class for parsing Admin REST API output

---
+++ Utils for PBI/Models/ServiceModels/ServiceMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils_for_PBI.Models.ReportMetadata
+{
+    public class ServiceMetadata
+    {
+        public string workspaceId { get; set; }
+        public string workspaceName { get; set; }
+        public string reportId { get; set; }
+        public string reportName { get; set; }
+        public string datasetId { get; set; }
+    }
+}
--- Utils for PBI/Services/ReportServices/GetServiceMetadata.cs
+++ Utils for PBI/Services/ReportServices/GetServiceMetadata.cs
@@ -1,22 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management.Automation.Language;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Utils_for_PBI.Services.ReportServices
 {
     public class GetServiceMetadata
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public GetServiceMetadata()
         {
 
+
         }
 
-        public void FetchAdminMetadata()
+        public static async Task<String> FetchAdminMetadata(string AccessToken)
         {
+            var serviceMetadataAPIURL = $"{Constants.PowerBIAdminAPIURL}/groups?$expand=reports&$top=1000";
 
+            using HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, serviceMetadataAPIURL);
+            requestMessage.Headers.Add("Authorization", AccessToken);
+            
+            using HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            JsonDocument jsonDocument = JsonDocument.Parse(responseContent);
+            JsonElement workspaces = jsonDocument.RootElement.GetProperty("value");
+
+            foreach (JsonElement workspace in workspaces.EnumerateArray())
+            {
+                JsonElement reportItems = workspace.GetProperty("reports");
+
+                foreach (JsonElement reportItem in reportItems.EnumerateArray())
+                {
+
+                }
+
+            }
+
+            return responseContent;
         }
-
     }
 }
